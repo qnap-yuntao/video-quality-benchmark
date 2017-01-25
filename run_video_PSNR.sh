5fdb76bcfd8aa440a78b801d4fabe5b497afc03a
qnap-yuntao <taochen@qnap.com>
2017-01-25 15:29:09 +0800

    tao push

--- run_video_PSNR.sh
+++ run_video_PSNR.sh
@@ -10,7 +10,6 @@
 # reference : xxx_1440p_10M_gst_4.5M-cbr-bf2.mp4
 # reference : xxx_1440p_10M_gst_4.5M-cbr-bf2-tune1.mp4
 
-
 datetime=$(date +%Y%m%d_%H%M%S)
 result=$datetime"_result.txt"
 result_output=$datetime"_result_output.txt"
@@ -27,14 +26,13 @@ result_output=$datetime"_result_output.txt"
 # input_arr=(xxx_1440p_10M_skylake-1080-4500k-x264-sdse.mp4 xxx_1440p_10M_skylake-1080-unlimited-x264-sdse.mp4)
 # reference_arr=(xxx_1440p_10M_kabylake-1080-4500k-vaapi-hdhe.mp4 xxx_1440p_10M_kabylake-1080-4500k-vaapi-sdhe.mp4)
 
-# test all data
-# input_arr=(xxx_1440p_10M_skylake-1080-4500k-x264-sdse.mp4 xxx_1440p_10M_skylake-1080-unlimited-x264-sdse.mp4)
-# reference_arr=(xxx_1440p_10M_skylake-1080-4500k-vaapi-hdhe.mp4 xxx_1440p_10M_skylake-1080-4500k-vaapi-sdhe.mp4 xxx_1440p_10M-m2000-1080-4500k-nvenc-hdhe-vbr.mp4 xxx_1440p_10M-m2000-1080-4500k-nvenc-hdhe-cbr.mp4 xxx_1440p_10M_kabylake-1080-4500k-vaapi-hdhe.mp4 xxx_1440p_10M_kabylake-1080-4500k-vaapi-sdhe.mp4)
-
 # test gstreamer data 20170125
-input_arr=(xxx_1440p_10M_skylake-1080-4500k-x264-sdse.mp4 xxx_1440p_10M_skylake-1080-unlimited-x264-sdse.mp4)
-reference_arr=(xxx_1440p_10M_gst_4.5M-cbr.mp4 xxx_1440p_10M_gst_4.5M-cbr-bf2.mp4 xxx_1440p_10M_gst_4.5M-cbr-bf2-tune1.mp4)
+# input_arr=(xxx_1440p_10M_skylake-1080-4500k-x264-sdse.mp4 xxx_1440p_10M_skylake-1080-unlimited-x264-sdse.mp4)
+# reference_arr=(xxx_1440p_10M_gst_4.5M-cbr.mp4 xxx_1440p_10M_gst_4.5M-cbr-bf2.mp4 xxx_1440p_10M_gst_4.5M-cbr-bf2-tune1.mp4)
 
+# test all data
+input_arr=(xxx_1440p_10M_skylake-1080-4500k-x264-sdse.mp4 xxx_1440p_10M_skylake-1080-unlimited-x264-sdse.mp4)
+reference_arr=(xxx_1440p_10M_skylake-1080-4500k-vaapi-hdhe.mp4 xxx_1440p_10M_skylake-1080-4500k-vaapi-sdhe.mp4 xxx_1440p_10M-m2000-1080-4500k-nvenc-hdhe-vbr.mp4 xxx_1440p_10M-m2000-1080-4500k-nvenc-hdhe-cbr.mp4 xxx_1440p_10M_kabylake-1080-4500k-vaapi-hdhe.mp4 xxx_1440p_10M_kabylake-1080-4500k-vaapi-sdhe.mp4 xxx_1440p_10M_gst_4.5M-cbr.mp4 xxx_1440p_10M_gst_4.5M-cbr-bf2.mp4 xxx_1440p_10M_gst_4.5M-cbr-bf2-tune1.mp4)
 
 for input in ${input_arr[@]};
 do
@@ -47,8 +45,8 @@ do
         echo "input "$input >> $result_output
         echo "reference "$reference >> $result_output
         echo $'\n' >> $result_output
-        ffmpeg -i $input -i $reference -filter_complex psnr -f null - 1>> $result 2>> temp.txt
-        # ffmpeg -i main.mpg -i ref.mpg -lavfi  "ssim;[0:v][1:v]psnr" -f null - 1>> $result 2>> temp.txt
+        # ffmpeg -i $input -i $reference -filter_complex psnr -f null - 1>> $result 2>> temp.txt
+        ffmpeg -i $input -i $reference -lavfi  "ssim;[0:v][1:v]psnr" -f null - 1>> $result 2>> temp.txt
         grep 'Parsed' temp.txt >> $result_output
         rm temp.txt
     done
