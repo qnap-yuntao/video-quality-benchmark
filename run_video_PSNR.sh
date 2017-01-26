2fdb794fd5ed15a831864373c6419a1df54e8384
SparkingR <leo.fan@mirlab.org>
2017-01-26 11:50:00 +0800

    add ssim

--- run_video_PSNR.sh
+++ run_video_PSNR.sh
@@ -1,35 +1,8 @@
 # input : xxx_1440p_10M_skylake-1080-4500k-x264-sdse.mp4
 # input : xxx_1440p_10M_skylake-1080-unlimited-x264-sdse.mp4
-# reference : xxx_1440p_10M_skylake-1080-4500k-vaapi-hdhe.mp4
-# reference : xxx_1440p_10M_skylake-1080-4500k-vaapi-sdhe.mp4
-# reference : xxx_1440p_10M-m2000-1080-4500k-nvenc-hdhe-vbr.mp4
-# reference : xxx_1440p_10M-m2000-1080-4500k-nvenc-hdhe-cbr.mp4
-# reference : xxx_1440p_10M_kabylake-1080-4500k-vaapi-hdhe.mp4 
-# reference : xxx_1440p_10M_kabylake-1080-4500k-vaapi-sdhe.mp4
-# reference : xxx_1440p_10M_gst_4.5M-cbr.mp4
-# reference : xxx_1440p_10M_gst_4.5M-cbr-bf2.mp4
-# reference : xxx_1440p_10M_gst_4.5M-cbr-bf2-tune1.mp4
 
 datetime=$(date +%Y%m%d_%H%M%S)
 result=$datetime"_result.txt"
-
-# test data 20170123
-# input_arr=(xxx_1440p_10M_skylake-1080-4500k-x264-sdse.mp4 xxx_1440p_10M_skylake-1080-unlimited-x264-sdse.mp4)
-# reference_arr=(xxx_1440p_10M_skylake-1080-4500k-vaapi-hdhe.mp4 xxx_1440p_10M_skylake-1080-4500k-vaapi-sdhe.mp4 xxx_1440p_10M-m2000-1080-4500k-nvenc-hdhe-vbr.mp4 xxx_1440p_10M-m2000-1080-4500k-nvenc-hdhe-cbr.mp4)
-
-# test one raw data
-#input_arr=(xxx_1440p_10M_skylake-1080-4500k-x264-sdse.mp4)
-#reference_arr=(xxx_1440p_10M_skylake-1080-4500k-vaapi-hdhe.mp4)
-
-# test new data 20170124
-# input_arr=(xxx_1440p_10M_skylake-1080-4500k-x264-sdse.mp4 xxx_1440p_10M_skylake-1080-unlimited-x264-sdse.mp4)
-# reference_arr=(xxx_1440p_10M_kabylake-1080-4500k-vaapi-hdhe.mp4 xxx_1440p_10M_kabylake-1080-4500k-vaapi-sdhe.mp4)
-
-# test gstreamer data 20170125
-# input_arr=(xxx_1440p_10M_skylake-1080-4500k-x264-sdse.mp4 xxx_1440p_10M_skylake-1080-unlimited-x264-sdse.mp4)
-# reference_arr=(xxx_1440p_10M_gst_4.5M-cbr.mp4 xxx_1440p_10M_gst_4.5M-cbr-bf2.mp4 xxx_1440p_10M_gst_4.5M-cbr-bf2-tune1.mp4)
-
-# test all data
 
 fileExtension='mp4'
 sourceIdf='x264'
@@ -43,16 +16,17 @@ do
     for reference in ${referenceArray[@]}
     do
         echo $datetime
-        echo $'\ninput : \t'$input
-        echo $'reference : \t'$reference
         echo $'\n====================================================================\n'
         echo $'\n====================================================================\n' >> $result.txt
+        echo $'\ninput : \t'$input
+        echo $'reference : \t'$reference
         echo "ffmpeg start"
-        ffmpeg -i $input -i $reference -filter_complex psnr -f null - 2>> temp.txt
+        #ffmpeg -i $input -i $reference -filter_complex psnr -f null - 2>> temp.txt
+        ffmpeg -i $input -i $reference -lavfi  "ssim;[0:v][1:v]psnr" -f null - 2>> temp.txt
         echo "ffmpeg end"
-        grep 'Input\|Parsed_psnr' temp.txt | awk 'NR==1{print "input: \t\t"  $5} NR==2{print "reference: \t" $5} NR==3{print}' >> $result.txt
+        echo $'\n'
+        grep 'Input\|Parsed_psnr|Parsed_ssim' temp.txt | awk 'NR==1{print "input: \t\t"  $5} NR==2{print "reference: \t" $5} NR==3{print} NR==4{print}' >> $result.txt
         rm temp.txt
-        # ffmpeg -i $input -i $reference -lavfi  "ssim;[0:v][1:v]psnr" -f null - 1>> $result 2>> temp.txt
 
     done
 done
