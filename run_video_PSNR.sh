36f267d55dcabb272d09ebd4046d6ad72f638161
SparkingR <leo.fan@mirlab.org>
2017-01-26 17:51:14 +0800

    minor change

--- run_video_PSNR.sh
+++ run_video_PSNR.sh
@@ -11,15 +11,21 @@ sourceFile='xxx_1440p_10M.mp4'
 inputArray=($(ls | grep -i $sourceIdf | grep -i $fileExtension))
 referenceArray=($(ls | grep -v $sourceIdf | grep -v $sourceFile | grep -i $fileExtension))
 
+echo $datetime
+
 for input in ${inputArray[@]}
 do
     for reference in ${referenceArray[@]}
     do
-        echo $datetime
-        echo -e "\n\n====================================================================\n" | tee -a $result
-        #ffmpeg -i $input -i $reference -filter_complex psnr -f null - 2>> temp.txt
+        echo -e "\n\n====================================================================\n" | \
+        tee -a $result
+
+        #ffmpeg -i $input -i $reference -filter_complex psnr -f null - 2 -
         ffmpeg -i $input -i $reference -lavfi  "ssim;[0:v][1:v]psnr" -f null - |& \
-        grep 'Input\|Parsed_psnr\|Parsed_ssim' | awk 'NR==1{print "input: \t\t"  $5} NR==2{print "reference: \t" $5} NR==3{print} NR==4{print}' | tee -a $result
+
+        grep 'Input\|Parsed_psnr\|Parsed_ssim' | \
+        awk 'NR==1{print "input: \t\t"  $5} NR==2{print "reference: \t" $5} NR==3{print} NR==4{print}' | \
+        tee -a $result
     done
 done
 
