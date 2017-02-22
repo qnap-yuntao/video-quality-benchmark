68d77fd585ce122b455ebb29c49f0ab6e496e30d
qnap-yuntao <taochen@qnap.com>
2017-02-22 12:26:41 +0800

    todo solve issue newline replacement

---
+++ test_ssim_psnr_video_benchmark.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 *-*
+import os
+import time
+
+if __name__=="__main__":
+
+    psnr_command = """ffmpeg -i xxx/xxx_1080p_10M_15sec.mp4 -i xxx/xxx_1080p_10M_dj2_result_tmp.mp4 -lavfi \"ssim;[0:v][1:v]psnr\" -f null - 2>&1 | \
+grep 'Parsed_psnr\|Parsed_ssim' | \
+awk 'NR==1{print $5","$6","$7","$8","} NR==2{print $5","$6","$7","$8","}' | sed 's/[A-Za-z]*:/\ /g' | \
+tee -a output.csv"""
+
+    print psnr_command
+
+    os.system(psnr_command)
--- video-quality-benchmark-lab1.py
+++ video-quality-benchmark-lab1.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 *-*
 import os
-
+import time
 
 if __name__=="__main__":
 
@@ -48,8 +48,12 @@ if __name__=="__main__":
 
     print vaapi_param_arr[0]
 
-    command = """docker exec dj2transcode2 bash -c 'dj2-transcode -hwaccel vaapi -hwaccel_output_format vaapi -vaapi_device /dev/dri/renderD128 \
--i /data/xxx/xxx_1440p_10M.mp4 -vf \"format=vaapi|nv12,hwupload,scale_vaapi=w=1920:h=1080\" -c:v h264_vaapi \
+    os.system("""echo 'g, bf, i_qfactor, i_qoffset, b_qfactor, b_qoffset, qmin, qmax, preset,\
+SSIM_Y, SSIM_U, SSIM_V, SSIM_All, PSNR_y, PSNR_u, PSNR_v, PSNR_average, Elapsed_Time, File_Size' > output.csv""")
+
+
+    transcode_command = """docker exec dj2transcode2 bash -c 'dj2-transcode -hwaccel vaapi -hwaccel_output_format vaapi -vaapi_device /dev/dri/renderD128 \
+-i /data/xxx/xxx_1440p_10M_15sec.mp4 -vf \"format=vaapi|nv12,hwupload,scale_vaapi=w=1920:h=1080\" -c:v h264_vaapi \
 -g {} \
 -bf {} \
 -i_qfactor {} -i_qoffset {} \
@@ -64,12 +68,33 @@ if __name__=="__main__":
 -maxrate 12000k -bufsize 2000k \
 -minrate 5500k \
 -me_method full \
--c:a copy -f mp4 /data/xxx/xxx_1440p_10M_dj2_leo_test.mp4'""".format(vaapi_param_arr[0][0], vaapi_param_arr[0][1], vaapi_param_arr[0][2], vaapi_param_arr[0][3], vaapi_param_arr[0][4], vaapi_param_arr[0][5], vaapi_param_arr[0][6], vaapi_param_arr[0][7], vaapi_param_arr[0][8])
+-c:a copy -f mp4 /data/xxx/xxx_1080p_10M_dj2_result_tmp.mp4'""".format(vaapi_param_arr[0][0], vaapi_param_arr[0][1], vaapi_param_arr[0][2], vaapi_param_arr[0][3], vaapi_param_arr[0][4], vaapi_param_arr[0][5], vaapi_param_arr[0][6], vaapi_param_arr[0][7], vaapi_param_arr[0][8])
+    print transcode_command
 
-    print command
+    os.system("echo -n \"{}, {}, {}, {}, {}, {}, {}, {}, {},\" >> output.csv".format(vaapi_param_arr[0][0], vaapi_param_arr[0][1], vaapi_param_arr[0][2], vaapi_param_arr[0][3], vaapi_param_arr[0][4], vaapi_param_arr[0][5], vaapi_param_arr[0][6], vaapi_param_arr[0][7], vaapi_param_arr[0][8]))
 
-    #os.system(command)
- 
+    start_time = time.time()
+    os.system(transcode_command)
+    elapsed_time = time.time() - start_time
+
+    psnr_command = """ffmpeg -i xxx/xxx_1080p_10M_15sec.mp4 -i xxx/xxx_1080p_10M_dj2_result_tmp.mp4 -lavfi  \"ssim;[0:v][1:v]psnr\" -f null - 2>&1 \
+grep 'Parsed_psnr\|Parsed_ssim' | \
+awk 'NR==1{print $5", "$6", "$7", "$8", "} NR==2{print $5", "$6", "$7", "$8", "}' | sed 's/[A-Za-z]*:/\ /g' | \
+tee -a output.csv"""
+
+    print psnr_command
+
+    os.system(psnr_command)
+
+    add_elapsed_time_command = """ echo -n " {}," >> output.csv """.format(elapsed_time)
+
+    os.system(add_elapsed_time_command)
+
+    add_file_size_command = """ls xxx -l | grep "xxx_1080p_10M_dj2_result_tmp.mp4" | awk '{print $5/1024/1024}' >>  output.csv"""
+    
+    os.system(add_file_size_command)
+
+    os.system("echo '\n' >> output.csv")
 
 
 
